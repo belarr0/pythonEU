f515b1886831c64852b34b05282c2e42c3d3070f
belarr0 <belarr00ss@gmail.com>
2022-10-24 20:45:59 +0300

    added new task

---
+++ merge_sort_HW/main.py
@@ -0,0 +1,26 @@
+def merge_two_list(a, b):
+    c = []
+    i = j = 0
+    while i < len(a) and j < len(b):
+        if a[i] < b[j]:
+            c.append(a[i])
+            i += 1
+        else:
+            c.append(b[j])
+            j += 1
+    if i < len(a):
+        c += a[i:]
+    if j < len(b):
+        c += b[j:]
+    return c
+
+def merge_sort(s):
+    if len(s) == 1:
+        return s
+    middle = len(s) // 2
+    left = merge_sort(s[:middle])
+    right = merge_sort(s[middle:])
+    return merge_two_list(left, right)
+
+print(*merge_sort([7, 5, 2, 3, 9, 8, 6]))
+#print(merge_sort([7, 5, 2, 3, 9, 8, 6]))
