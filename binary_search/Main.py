4ab1de7b3cc23a0e5d60609797121b9d080edfd9
belarr0 <belarr00ss@gmail.com>
2022-11-26 18:38:55 +0200

    modified randomizer and output

--- binary_search/Main.py
+++ binary_search/Main.py
@@ -11,15 +11,15 @@ def bubble_sort(nums):
                 nums[i], nums[i + 1] = nums[i + 1], nums[i] #міняємо місцями
                 swapped = True #для наступної ітерації
 
-num_list = random.sample(range(0, 101), 100)
+num_list = random.sample(range(-50, 101), 100)
+print("Before: ", num_list)
 bubble_sort(num_list)
-print(num_list)
+print("After: ", num_list)
 
 def binary_search(num_list, x):
 	low = 0
 	high = len(num_list) - 1
 	mid = 0
-
 	while low <= high:
 		mid = (high + low) // 2
 		if num_list[mid] < x:
@@ -36,6 +36,6 @@ def binary_search(num_list, x):
 result = binary_search(num_list, x)
 
 if result != -1:
-	print("Element  at index", str(result))
+	print("Element at index", str(result))
 else:
 	print("Element is not in array")
